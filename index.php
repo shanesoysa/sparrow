ddbce03f9fe1a9c74b58c0d039503b7b22ba6ed0
Shane soyza <shanesanjaya@gmail.com>
2020-11-12 14:50:17 -0500

    complete

---
+++ index.php
@@ -0,0 +1,615 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!--
+New Event
+http://www.templatemo.com/tm-486-new-event
+-->
+<title>Sparrow</title>
+<meta name="description" content="">
+<meta name="author" content="">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="css/owl.theme.css">
+<link rel="stylesheet" href="css/owl.carousel.css">
+
+<!-- Main css -->
+<link rel="stylesheet" href="css/style.css">
+
+<!-- Google Font -->
+<link href='https://fonts.googleapis.com/css?family=Poppins:400,500,600' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css">
+<link rel="icon" type="image/png" href="images/lOGO1-3.png"/>
+</head>
+<body data-spy="scroll" data-offset="50" data-target=".navbar-collapse">
+
+<!-- =========================
+     PRE LOADER       
+============================== -->
+<div class="preloader">
+
+	<!-- <div class="sk-rotating-plane"></div> -->
+	<img  src="images/lOGO1-3.png" class="img-responsive" alt="sparrow">
+
+
+</div>
+
+
+<!-- =========================
+     NAVIGATION LINKS     
+============================== -->
+<div class="navbar navbar-fixed-top custom-navbar" role="navigation">
+	<div class="container">
+
+		<!-- navbar header -->
+		<div class="navbar-header">
+			<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+			</button>
+			<img src="images/lOGO1-3.png" style="max-width: 200px;" class="img-responsive" alt="sparrow">
+		</div>
+
+		<div class="collapse navbar-collapse">
+
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#intro" class="smoothScroll">Home</a></li>
+				<li><a href="#overview" class="smoothScroll">About</a></li>
+				<li><a href="#speakers" class="smoothScroll">Product</a></li>
+				<li><a href="#speakers1" class="smoothScroll">Our Team</a></li>
+				<li><a href="#contact" class="smoothScroll">Contact</a></li>
+			</ul>
+
+		</div>
+
+	</div>
+</div>
+
+
+<!-- =========================
+    INTRO SECTION   
+============================== -->
+<section id="carousel" class="parallax-section">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12">
+				<!-- <h3 class="wow bounceIn" data-wow-delay="0.9s">July 22 - 26 in San Francisco, CA</h3>
+				<h1 class="wow fadeInUp" data-wow-delay="1.6s">Web Design Conference</h1> -->
+
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <div class="carousel-inner">
+    <div class="active item">
+      <img src="images/IMG_3833.jpeg" style="height: 500px;
+width: 100%; overflow:hidden; object-fit: cover;  filter: brightness(50%); " alt="...">
+      <div class="carousel-content">
+        <!-- <p>TEST</p> -->
+      </div>
+      <div class="carousel-caption">
+	  <center><img src="images/lOGO1-3.png" style="" class="img-responsive" alt="sparrow"></center>
+	  <h2>We maintain&#8230;</h2>
+		<p style="color:white">
+		Corporate Environmental Responsibility<br />
+		Sustainable Manufacturing Initiatives<br />
+		Conservation Of Energy &#038; Natural Resources
+		</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="images/IMG_3835.jpeg" style="height: 500px;
+width: 100%;  object-fit: cover;  filter: brightness(50%); " alt="...">
+      <div class="carousel-caption">
+	  <center><img src="images/lOGO1-3.png" style="" class="img-responsive" alt="sparrow"></center>
+
+        <h1>We are going in green</h1>
+      </div>
+    </div>
+    <div class="item">
+      <img src="images/s4-2.jpg" style="height: 500px;
+width: 100%;  object-fit: cover;  filter: brightness(50%); " alt="...">
+      <div class="carousel-caption">
+	  <center><img src="images/lOGO1-3.png" style="" class="img-responsive" alt="sparrow"></center>
+
+        <h1>Vivid & Radiant colour</h1>
+      </div>
+    </div>
+  </div>
+
+			</div>
+
+
+		</div>
+	</div>
+</section>
+
+
+<!-- =========================
+    OVERVIEW SECTION   
+============================== -->
+<section id="overview" class="parallax-section">
+	<div class="container">
+		<div class="row">
+
+			<div class="wow fadeInUp col-md-12 col-sm-12 " data-wow-delay="0.6s">
+				<h2 class="text-center">About Colour Pigment</h2>
+				<p  class="text-center" style="color: #fc9d03;font-size: large;">
+					The primary colors of pigment are
+					Red, Blue, Yellow, Green, White, Black and Brown.
+					(Commonly Simplified as Red, Yellow, and Blue)
+					Pigments are chemicals that absorb selective
+					wavelengths—they prevent certain wavelengths of
+					light from being transmitted or reflected.
+				</p>
+			</div>
+<!-- 					
+			<div class="wow fadeInUp col-md-6 col-sm-6" data-wow-delay="0.9s">
+				<img src="images/overview-img.jpg" class="img-responsive" alt="Overview">
+			</div> -->
+
+		</div>
+	</div>
+</section>
+
+
+
+
+
+<!-- =========================
+   Who is Sparrow?
+============================== -->
+<section id="video" class="parallax-section" name="About">
+	<div class="container">
+		<div class="row">
+			<div class="wow fadeInUp col-md-6 col-sm-10" data-wow-delay="1.6s">
+				<div class="embed-responsive embed-responsive-16by9">
+					<img style="background: linear-gradient(black, #fc9d03);" src="images/Bird_PNG_Clipart-65-1.png" height="310px" alt="sparrow">
+				</div>
+			</div>
+
+			<div class="wow fadeInUp col-md-6 col-sm-10" data-wow-delay="1.3s">
+				<h2>Who is Sparrow?</h2>
+				<p>Sparrow is Trade Mark Of The Sanju Color pigments (Pvt) Ltd . Sparrow was found in 1995.Our sole focus is Pigments. Sparrow with sources that consistently offer superior quality and excellent value. We shop the world, sourcing pigments from dozens of factories in Sri Lanka to bring you the most comprehensive pigment line available. We are the largest importer of Organic pigments In Sri Lanka. Sparrow earned customers’ highest rating. Our customers like what we offer.</p>
+			</div>
+
+
+		</div>
+	</div>
+</section>
+
+
+<!-- =========================
+    SPEAKERS SECTION   
+============================== -->
+<section id="speakers" class="parallax-section">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12 wow bounceIn">
+				<div class="section-title">
+					<h2>Our Products</h2>
+					<p>Pigment Products</p>
+				</div>
+			</div>
+
+			
+			<div id="owl-speakers" class="owl-carousel">
+
+
+				<div class="item wow fadeInUp col-md-3 col-sm-3" data-wow-delay="0.6s">
+					<div class="speakers-wrapper">
+						<img src="images/16.jpg" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Holland Red(614B)</h3>
+							</div>
+					</div>
+				</div>
+
+				<div class="item wow fadeInUp col-md-3 col-sm-3" data-wow-delay="0.9s">
+					<div class="speakers-wrapper">
+						<img src="images/12.jpg" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Holland Yellow(301) </h3>
+							</div>
+					</div>
+				</div>
+
+				<div class="item wow fadeInUp col-md-3 col-sm-3" data-wow-delay="0.6s">
+					<div class="speakers-wrapper">
+						<img src="images/17.jpg" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Holland Blue(466) </h3>
+							</div>
+					</div>
+				</div>
+
+				<div class="item wow fadeInUp col-md-3 col-sm-3" data-wow-delay="0.6s">
+					<div class="speakers-wrapper">
+						<img src="images/15.jpg" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Holland Green(805)</h3>
+							</div>
+					</div>
+				</div>
+				
+			</div>
+			<a href="/sparrow/items.php" class="btn pull-right" style="background-color: #fc9d03;"> Explore More <i class="fa fa-arrow-right"></i></a>
+
+
+		</div>
+	</div>
+</section>
+
+<!-- =========================
+    DETAIL SECTION   
+============================== -->
+<section id="detail" class="parallax-section">
+	<div class="container">
+		<div class="wow fadeInUp col-md-12 col-sm-12" data-wow-delay="0.6s">
+			<div class="section-title">
+				<h2>Other Products</h2>
+			</div>
+		</div>
+		<div class="row">
+
+			<div class="wow fadeInLeft col-md-4 col-sm-4" data-wow-delay="0.3s">
+				<img src="images/supreme-A4.jpg" class="img-responsive" alt="speakers">
+			
+			</div>
+
+			<div class="wow fadeInUp col-md-4 col-sm-4" data-wow-delay="0.6s">
+				<img src="images/Turpentine-1.jpg" class="img-responsive" alt="speakers">
+			</div>
+
+			<div class="wow fadeInRight col-md-4 col-sm-4" data-wow-delay="0.9s">
+				<img src="images/wood-presertative-1.jpg" class="img-responsive" alt="speakers">
+			</div>
+
+		</div>
+	</div>
+</section>
+
+<section id="video" class="parallax-section">
+	<div class="container">
+		<div class="row">
+			
+
+			<div class="wow fadeInUp col-md-6 col-sm-10" data-wow-delay="1.3s">
+				<h2>Pigments from "Sparrow"</h2>
+				
+				<p>Sparrow has been dedicated to serving pigment consuming industries since 1995. Our employees are come from pigment manufacturing backgrounds and have expert technical knowledge to help you with your pigment requirements .</p>
+				<ul type="bullet">
+					<li><h4>On Time Delivery</h4></li>
+					<li><h4>Product Quality</h4></li>
+					<li><h4>Price leader of the pigment industry </h4>
+						
+					</li>
+					<li><h4>Eco Friendly Products </h4></li>
+
+				</ul>
+			</div>
+
+			<div class="wow fadeInUp col-md-6 col-sm-10" data-wow-delay="1.6s">
+				<div class="embed-responsive embed-responsive-16by9">
+					<img src="images/w1-2.jpg" class="img-responsive" alt="sparrow">
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+</section>
+
+<section id="detail" class="parallax-section">
+	<div class="container">
+		<div class="wow fadeInUp col-md-12 col-sm-12" data-wow-delay="0.6s">
+			<div class="section-title">
+				<h2>2000+ Satisfied Customers</h2>
+			</div>
+		</div>
+		<div class="row">
+
+			<div class="wow fadeInLeft col-md-6 col-sm-12" data-wow-delay="0.3s">
+				<img src="images/user.png" style="height: 150px; border-radius: 50%;" >
+				<p style="font-size: large;">“Sparrow is well known to me for the past 15 years.
+					They are the one of  best Pigments & Other
+					Accessories supplier for Us.”</p>
+				<h5>Upul Hardware Peradeniya</h5>			
+			</div>
+
+			<div class="wow fadeInRight col-md-6 col-sm-12" data-wow-delay="0.3s">
+			<img src="images/user.png"  style="height: 150px; border-radius: 50%;">
+
+				<p style="font-size: large;">“Sparrow is  best supplier of us since 2005 Sparrow products  have highly demand In pigment Industry it is a reason for our long time relationship.”</p>
+				<h5>Samantha Hardware</h5>			
+			</div>
+
+		</div>
+	</div>
+</section>
+
+
+<section id="speakers1" class="parallax-section">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12 wow bounceIn">
+				<div class="section-title">
+					<h2>Our Team</h2>
+				</div>
+			</div>
+
+			<!-- Testimonial Owl Carousel section
+			================================================== -->
+			<div id="owl-staff" class="owl-carousel">
+
+				<div class="item wow fadeInUp col-md-4 col-sm-4" data-wow-delay="0.6s">
+					<div class="speakers-wrapper">
+						<img src="images/user.png" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Mrs S D K D Chandana</h3>
+								<h6>Chairman/Managing Director</h6>
+							</div>
+					</div>
+				</div>
+
+				<div class="item wow fadeInUp col-md-4 col-sm-4" data-wow-delay="0.9s">
+					<div class="speakers-wrapper">
+						<img src="images/staff3.jpg" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Krishanthi Galabadage</h3>
+								<h6>Alternative Director</h6>
+							</div>
+					</div>
+				</div>
+
+
+				<div class="item wow fadeInUp col-md-4 col-sm-4" data-wow-delay="0.6s">
+					<div class="speakers-wrapper">
+						<img src="images/staff4.jpg" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Vidyani Danister</h3>
+								<h6>Administrator</h6>
+							</div>
+					</div>
+				</div>
+				
+			</div>
+
+		</div>
+	</div>
+</section>
+
+
+
+<!-- =========================
+    CONTACT SECTION   
+============================== -->
+<section id="contact" class="parallax-section">
+	<div class="container">
+		<div class="row">
+
+			<div class="wow fadeInUp col-md-offset-1 col-md-5 col-sm-6" data-wow-delay="0.6s">
+				<div class="contact_des" style="color: floralwhite;">
+				<h2><i class="fa fa-map-marker" aria-hidden="true"></i>
+No.73.Ragama Road, Kandana,Sri Lanka</h2><br>
+					<h3><i class="fa fa-phone"></i> +94 11 2245621</h3><br>
+					<h3><i class="fa fa-mobile"></i> +94 718684021</h3><br>
+					<h3><i class="fa fa-fax" aria-hidden="true"></i>
+ +94 112233655</h3><br>
+					<h3><i class="fa fa-envelope"></i> contact@divifinance.com</h3><br>
+					<h3><i class="fa fa-envelope"></i> info@sparrow.lk</h3><br>
+
+
+				</div>
+			</div>
+
+			<div class="wow fadeInUp col-md-5 col-sm-6" data-wow-delay="0.9s">
+				<div class="contact_detail">
+					<div class="section-title">
+						<h2>Place An Order Online</h2>
+					</div>
+					<form id="form-Itemorder" name="form-Itemorder" action="http://127.0.0.1:8000/api/store" method="post">
+						<input name="name" type="text" class="form-control" id="name" placeholder="Name" required>
+						<input name="address" type="text" class="form-control" id="address" placeholder="Location address">
+
+					  	<input name="email" type="email" class="form-control" id="email" placeholder="Email">
+						  <input name="mobile" class="form-control" id="mobile" pattern="07[1,2,5,6,7,8][0-9]+" maxlength="10" placeholder="Mobile Number" required></input>
+						<select  class="form-control" id="item" name="item" >
+							<option selected disabled>Select one or more items</option>
+						</select>
+						<input type="number" min="1" name="quantity" lass="form-control" id="quantity" placeholder="Quantity"></input>
+
+						<table class="table" id="items">
+							<tr>
+								<th>Item</th>
+								<th>Quantity</th>
+							</tr>
+							<tbody>
+								<tr>
+									<td colspan="2">
+									<button class="form-group pull-right" id="add" name="add"><i class="fa fa-plus"></i></button>
+									</td>
+								</tr>
+
+							</tbody>
+
+						</table>
+						<div class="col-md-6 col-sm-10">
+
+							<input name="submit" type="submit" class="form-control pull-right" id="submit" value="Submit">
+							</form>
+
+						</div>
+
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+
+
+<!-- =========================
+    FOOTER SECTION   
+============================== -->
+<footer>
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12">
+				<p class="wow fadeInUp" data-wow-delay="0.6s">Copyright &copy; 2020 Sparrow
+                    
+                    | Design: <a rel="nofollow" href="https://www.dtinnovations.lk/" target="_parent">DTInnovations</a></p>
+
+				<ul class="social-icon">
+					<li><a href="https://www.facebook.com/dtinnovations.lk/" class="fa fa-facebook wow fadeInUp" data-wow-delay="1s"></a></li>
+					<li><a href="https://twitter.com/dti_17" class="fa fa-twitter wow fadeInUp" data-wow-delay="1.3s"></a></li>
+					<li><a href="https://www.instagram.com/dtinnovations_sw/?hl=en" class="fa fa-instagram wow fadeInUp" data-wow-delay="1.6s"></a></li>
+				
+				</ul>
+
+			</div>
+			
+		</div>
+	</div>
+</footer>
+
+
+<!-- Back top -->
+<a href="#back-top" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+
+<!-- =========================
+     SCRIPTS   
+============================== -->
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.parallax.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/custom.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
+
+<script>
+
+	var orders = [];
+
+			
+	$(function () {
+		
+		$.ajax({
+        url: 'http://127.0.0.1:8000/api/items',
+        method: 'get',
+        error: function (e){
+            console.error(e);
+        },
+        success: function (r){
+            $.each(r, function (k, v) {
+              $('#item').append($('<option>', { 
+                value: v.id,
+               text : v.name 
+             }));
+            
+            });
+
+            
+        }
+    });
+	
+        $("#add").click(function (e) {
+		e.preventDefault();
+
+		var item=$('#item option:selected').text();
+		var itemid=$('#item').val();
+		var qty=$('#quantity').val();
+
+		orders.push(
+			{"id": itemid, "quantity": qty }
+		);
+		$("#items").append("<tr><td>"+item+"</td><td>"+qty+"</td></tr>");
+
+
+				
+		});
+		
+    })
+
+	$('#form-Itemorder').on('submit', function(e){
+      e.preventDefault();
+
+      let form_action = $(this).prop('action');
+    //   let form_data = $(this).serializeArray();
+      let form_id = $(this).prop('id');
+
+	  let name= $('#name').val();
+	  let email= $('#email').val();
+	  let mobile= $('#mobile').val();
+	  let address=  $('#address').val();
+
+	  	if(orders.length < 1 || orders == undefined){
+
+			alert("Please select Items You want to order");
+
+
+		} else {
+			$.ajax({
+			url: form_action,
+			data: {
+				name:name,
+				address:address,
+				email:email,
+				mobile:mobile,
+				orders:orders,
+			},
+			method: 'post',
+			error: function (e){
+			alert("something went wrong");
+			},
+			success: function (r){
+				alert("Your order was placed");
+			
+			$('#'+form_id).trigger('reset');
+			 setTimeout(function() {
+									window.location.reload();
+								}, 1500);
+			}
+		});
+
+		}
+    });
+
+	jQuery.validator.setDefaults({
+  	debug: true,
+	success: "valid"
+	});
+	$( "#mobile" ).validate({
+	rules: {
+		field: {
+		required: true,
+		number: true
+		}
+		}
+	});
+
+
+</script>
+
+</body>
+</html>
---
+++ items.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!--
+New Event
+http://www.templatemo.com/tm-486-new-event
+-->
+<title>Sparrow</title>
+<meta name="description" content="">
+<meta name="author" content="">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="css/owl.theme.css">
+<link rel="stylesheet" href="css/owl.carousel.css">
+
+<!-- Main css -->
+<link rel="stylesheet" href="css/style.css">
+
+<!-- Google Font -->
+<link href='https://fonts.googleapis.com/css?family=Poppins:400,500,600' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css">
+
+</head>
+<body data-spy="scroll" data-offset="50" data-target=".navbar-collapse">
+
+<!-- =========================
+     PRE LOADER       
+============================== -->
+<div class="preloader">
+
+	<div class="sk-rotating-plane"></div>
+
+</div>
+
+
+<!-- =========================
+     NAVIGATION LINKS     
+============================== -->
+<div class="navbar navbar-fixed-top custom-navbar" role="navigation">
+	<div class="container">
+
+		<!-- navbar header -->
+		<div class="navbar-header">
+			<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+			</button>
+			<img src="images/lOGO1-3.png" style="max-width: 200px;" class="img-responsive" alt="sparrow">
+		</div>
+
+		<div class="collapse navbar-collapse">
+
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="index.php" class="smoothScroll">Home</a></li>
+				<li><a href="index.php" class="smoothScroll">About</a></li>
+				<li><a href="index.php" class="smoothScroll">Product</a></li>
+				<li><a href="index.php" class="smoothScroll">Our Team</a></li>
+				<li><a href="index.php" class="smoothScroll">Contact</a></li>
+			</ul>
+
+		</div>
+
+	</div>
+</div>
+
+
+
+
+
+
+<!-- =========================
+    SPEAKERS SECTION   
+============================== -->
+<section id="speakers" class="parallax-section">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12 wow bounceIn">
+				<div class="section-title">
+					<h2>Our Products</h2>
+					<p>Pigment Products</p>
+				</div>
+			</div>
+
+			
+			<div id="owl-speakers" class="owl-carousel">
+
+
+				<div class="item wow fadeInUp col-md-3 col-sm-3" data-wow-delay="0.6s">
+					<div class="speakers-wrapper">
+						<img src="images/16.jpg" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Holland Red</h3>
+								<p>Superior Quality For A Quality Floor 15 KG of normal cement for 1 KG of Holland Red cement</p>
+								<h3>Available Package:</h3>
+								<p>500 G ,1 Kg,10 Kg,20 Kg,25 Kg, 45 Kg</p>
+							
+							</div>
+					</div>
+				</div>
+
+				<div class="item wow fadeInUp col-md-3 col-sm-3" data-wow-delay="0.9s">
+					<div class="speakers-wrapper">
+						<img src="images/12.jpg" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Holland Yellow </h3>
+								<p>Superior Quality For A Quality Floor 12 KG of normal cement for 1 KG of Holland Red cement</p>
+								<h3>Available Package:</h3>
+								<p>500 G ,10 Kg</p>
+
+							</div>
+					</div>
+				</div>
+
+				<div class="item wow fadeInUp col-md-3 col-sm-3" data-wow-delay="0.6s">
+					<div class="speakers-wrapper">
+						<img src="images/17.jpg" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Holland Blue</h3>
+								<p>Superior Quality For A Quality Floor 12 KG of normal cement for 1 KG of Holland Red cement</p>
+								<h3>Available Package:</h3>
+								<p>500 G ,1 Kg,10  Kg</p>
+							</div>
+					</div>
+				</div>
+
+				<div class="item wow fadeInUp col-md-3 col-sm-3" data-wow-delay="0.6s">
+					<div class="speakers-wrapper">
+						<img src="images/15.jpg" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Holland Green</h3>
+								<p>Superior Quality For A Quality Floor 12 KG of normal cement for 1 KG of Holland Red cement</p>
+								<h3>Available Package:</h3>
+								<p>500 G ,1 Kg,10 Kg</p>
+							</div>
+					</div>
+				</div>
+
+				<div class="item wow fadeInUp col-md-3 col-sm-3" data-wow-delay="0.6s">
+					<div class="speakers-wrapper">
+						<img src="images/11.jpg" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Holland Black</h3>
+								<p>Superior Quality For A Quality Floor 15 KG of normal cement for 1 KG of Holland Red cement</p>
+								<h3>Available Package:</h3>
+								<p>500 G ,1 Kg,10 Kg,20 Kg,25 Kg, 45 Kg</p>
+							</div>
+					</div>
+				</div>
+
+				<div class="item wow fadeInUp col-md-3 col-sm-3" data-wow-delay="0.9s">
+					<div class="speakers-wrapper">
+						<img src="images/18.jpg" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Holland Brown</h3>
+								<p>Superior Quality For A Quality Floor 12 KG of normal cement for 1 KG of Holland Red cement</p>
+								<h3>Available Package:</h3>
+								<p>500 G ,1 Kg,10 Kg,45 Kg</p>
+							</div>
+					</div>
+				</div>
+
+				<div class="item wow fadeInUp col-md-3 col-sm-3" data-wow-delay="0.6s">
+					<div class="speakers-wrapper">
+						<img src="images/14.jpg" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Holland Titanium</h3>
+								<p>Superior Quality For A Quality Floor 20 KG of normal cement for 1 KG of Holland Red cement</p>
+								<h3>Available Package:</h3>
+								<p>500 G ,1 Kg,10 Kg</p>
+							</div>
+					</div>
+				</div>
+
+				<div class="item wow fadeInUp col-md-3 col-sm-3" data-wow-delay="0.6s">
+					<div class="speakers-wrapper">
+						<img src="images/13.jpg" class="img-responsive" alt="speakers">
+							<div class="speakers-thumb">
+								<h3>Holland Orange</h3>
+								<p>Superior Quality For A Quality Floor 12 KG of normal cement for 1 KG of Holland Red cement</p>
+								<h3>Available Package:</h3>
+								<p>500 G ,10 Kg</p>
+							</div>
+					</div>
+				</div>
+				
+			</div>
+
+		</div>
+	</div>
+</section>
+
+
+
+<!-- =========================
+    FOOTER SECTION   
+============================== -->
+<footer>
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12">
+				<p class="wow fadeInUp" data-wow-delay="0.6s">Copyright &copy; 2020 Sparrow
+                    
+                    | Design: <a rel="nofollow" href="https://www.dtinnovations.lk/" target="_parent">DTInnovations</a></p>
+
+				<ul class="social-icon">
+					<li><a href="#" class="fa fa-facebook wow fadeInUp" data-wow-delay="1s"></a></li>
+					<li><a href="#" class="fa fa-twitter wow fadeInUp" data-wow-delay="1.3s"></a></li>
+					<li><a href="#" class="fa fa-dribbble wow fadeInUp" data-wow-delay="1.6s"></a></li>
+					<li><a href="#" class="fa fa-behance wow fadeInUp" data-wow-delay="1.9s"></a></li>
+					<li><a href="#" class="fa fa-google-plus wow fadeInUp" data-wow-delay="2s"></a></li>
+				</ul>
+
+			</div>
+			
+		</div>
+	</div>
+</footer>
+
+
+<!-- Back top -->
+<a href="#back-top" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+
+<!-- =========================
+     SCRIPTS   
+============================== -->
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.parallax.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/custom.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+
+</body>
+</html>
